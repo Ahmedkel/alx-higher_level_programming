b6dd5d43886d05de935e086f34a8e06f5e409dc4
Ahmedkel <ahmedkeloch@gmail.com>
2023-06-08 14:58:10 +0100

    2. How to calulate sum for args

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of numbers of arguments passed"""
+import sys
+
+args = sys.argv[1:]
+sum_of_args = sum(map(int, args))
+
+print(sum_of_args)
