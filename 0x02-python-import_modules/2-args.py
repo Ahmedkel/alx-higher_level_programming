0e3a54c49bef572ba61cd5bc3777dc5cc634eb32
Ahmedkel <ahmedkeloch@gmail.com>
2023-06-08 11:40:24 +0100

    2. How to make a script dynamic! for args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments"""
+    """Print the number of arguments passed in a list form"""
 import sys
 
 args = sys.argv[1:]
