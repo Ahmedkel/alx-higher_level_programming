f7228a8ac23d96d97f1e1c95c1fd53e9da0ff8dd
Ahmedkel <ahmedkeloch@gmail.com>
2023-06-08 11:34:53 +0100

    1. My first toolbox! calculator py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+from calculator_1 import *
 a = 10
 b = 5
-from calculator_1 import *
-print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+print('{} + {} = {}'.format(a, b, add(a, b)))
+print('{} - {} = {}'.format(a, b, sub(a, b)))
+print('{} * {} = {}'.format(a, b, mul(a, b)))
+print('{} / {} = {}'.format(a, b, div(a, b)))
