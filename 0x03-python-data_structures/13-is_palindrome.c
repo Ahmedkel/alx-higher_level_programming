f604b6cfc1531ba18b1a6adfe4dc090c645e51e7
Ahmedkel <ahmedkeloch@gmail.com>
2023-06-10 20:05:56 +0100

    Write a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,38 +1,44 @@
 #include"lists.h"
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to pointer to the head of the linked list
+ *
+ * Return: 1 if the linked list is a palindrome, 0 otherwise
+ */
 int is_palindrome(listint_t **head)
 {
-    listint_t *fast = *head;
-    listint_t *slow = *head;
-    listint_t *prev = NULL;
-    listint_t *next;
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	listint_t *prev = NULL;
+	listint_t *next;
 
-    /* Find the middle of the list using Floyd's cycle-finding algorithm */
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        prev = slow;
-        slow = slow->next;
-    }
+	/* Find the middle of the list using Floyd's cycle-finding algorithm */
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
 
-    /* Reverse the second half of the list */
-    while (slow != NULL)
-    {
-        next = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = next;
-    }
+	/* Reverse the second half of the list */
+	while (slow != NULL)
+	{
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
 
-    /* Compare the first half of the list with the reversed second half */
-    slow = prev;
-    fast = *head;
-    while (slow != NULL)
-    {
-        if (slow->n != fast->n)
-            return 0;
-        slow = slow->next;
-        fast = fast->next;
-    }
+	/* Compare the first half of the list with the reversed second half */
+	slow = prev;
+	fast = *head;
+	while (slow != NULL)
+	{
+		if (slow->n != fast->n)
+			return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
 
-    return 1;
+	return (1);
 }
