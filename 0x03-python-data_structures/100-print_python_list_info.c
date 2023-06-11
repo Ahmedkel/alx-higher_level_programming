f5ca02846d775698cc9589d6ae1416479a377454
Ahmedkel <ahmedkeloch@gmail.com>
2023-06-11 08:05:41 +0000

    cpython it's weird

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <Python.h>
 
 /**
@@ -6,40 +7,19 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size, i;
-	PyObject *item;
+    Py_ssize_t size, i;
+    PyListObject *list;
+    PyObject *item;
 
-	size = PyList_Size(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+    size = Py_SIZE(p);
+    printf("[*] Size of the Python List = %ld\n", size);
 
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+    list = (PyListObject *)p;
+    printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: ", i);
-
-		if (PyUnicode_Check(item))
-			printf("str\n");
-		else if (PyLong_Check(item))
-			printf("int\n");
-		else if (PyFloat_Check(item))
-			printf("float\n");
-		else if (PyList_Check(item))
-			printf("list\n");
-		else if (PyTuple_Check(item))
-			printf("tuple\n");
-		else if (PyDict_Check(item))
-			printf("dict\n");
-		else if (PyAnySet_Check(item))
-			printf("set\n");
-		else if (PyAnyIterator_Check(item))
-			printf("iter\n");
-		else if (PyModule_Check(item))
-			printf("module\n");
-		else if (PyCallable_Check(item))
-			printf("function\n");
-		else
-			printf("%s\n", Py_TYPE(item)->tp_name);
-	}
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
 }
