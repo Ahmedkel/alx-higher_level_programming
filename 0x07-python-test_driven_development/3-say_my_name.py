793dbb16b439053df64324047f109a1f42b05196
Ahmedkel <ahmedkeloch@gmail.com>
2023-07-05 19:58:54 +0100

    fixing and cleaning up the code

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""3-say_my_name.py"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first name> <last name>"
 
     Args:
         first_name (str): The first name.
-        last_name (str, optional): The last name. Defaults to "".
+        last_name (str, optional): The last name. The defaults to "".
 
     Raises:
-        TypeError: If first_name or last_name is not a string.
-
-    Returns:
-        None
+        TypeError: If either first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
