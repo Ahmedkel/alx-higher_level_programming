be025c69f30600ee81e29a16324870cde97a8232
Ahmedkel <ahmedkeloch@gmail.com>
2023-07-11 17:01:33 +0100

    Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" function that returns the dictionary. """
+
+
+def class_to_json(obj):
+    """ Function retuns dictionary """
+    dic = {}
+    if hasattr(obj, "__dict__"):
+        dic = obj.__dict__.copy()
+    return dic
