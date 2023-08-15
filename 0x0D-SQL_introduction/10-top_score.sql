eeec2eac84bc9ff521232f3d9a1671b8bde364c5
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-15 18:19:59 +0000

    lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Prints specific rows in order big to small of the table second_table in my MySQL server.
-SELECT score, name FROM second_table DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
