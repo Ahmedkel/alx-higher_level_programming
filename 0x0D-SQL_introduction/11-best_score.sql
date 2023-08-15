8f283cbe5203c92cace2dcd638b50f2e7b9cf9fd
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-15 18:35:12 +0000

    lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Prints specific rows in order big to small of the table second_table in my MySQL server.
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
