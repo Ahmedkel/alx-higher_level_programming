a5920d839960839d3fee7bcaf40f598515485d6c
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-15 18:59:44 +0000

    updates the score of Bob to 10 in the table second_table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
 -- update the value of row in a table in your MySQL server.
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESC;
+UPDATE second_table
+SET score >= 10
+WHERE name = Bob;
