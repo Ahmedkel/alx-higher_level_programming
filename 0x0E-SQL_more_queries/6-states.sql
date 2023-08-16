aaccbc296976274857de16ada6d7a40d3b320de9
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-16 17:57:51 +0100

    id INT unique, auto generated, can’t be null and is a primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- creates the database with id INT unique, auto generated, can’t be null and is a primary key hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256) NOT NULL);
