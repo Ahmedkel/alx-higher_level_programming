e147922961933fd19619bde61ede72db59275f19
Ahmedkel <ahmedkeloch@gmail.com>
2023-09-28 18:54:22 +0100

    POST request to the passed URL

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# Send a POST request to a URL passed as an argument,
+# and displays the body of the response
+
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
