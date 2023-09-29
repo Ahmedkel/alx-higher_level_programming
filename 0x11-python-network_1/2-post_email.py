4f11a3a2a3c2a90ade737060fb2d93950a3ac02d
Ahmedkel <ahmedkeloch@gmail.com>
2023-09-29 19:06:28 +0100

    script that takes in a URL and an email, sends a POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+"""
+Sends a POST request to the specified
+URL with the email as a parameter,
+and displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode()
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
