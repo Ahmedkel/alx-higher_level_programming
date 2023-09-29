a5a59084234b09565bbcfd947c41a6d8e72f383f
Ahmedkel <ahmedkeloch@gmail.com>
2023-09-29 19:35:12 +0100

    sends a request to the URL and displays the value of the variable

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""script that takes in a URL,
+sends a request to the URL and
+displays the value of the variable"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if 'X-Request-Id' in response.headers:
+        x_request_id = response.headers['X-Request-Id']
+        print(x_request_id)
