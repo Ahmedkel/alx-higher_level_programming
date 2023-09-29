8be7e4911f06b2ad6f6f3b920bc697e1d44a24aa
Ahmedkel <ahmedkeloch@gmail.com>
2023-09-29 19:43:08 +0100

    sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,11 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    value = {"email": sys.argv[2]}
 
-    data = {'email': email}
-
-    response = requests.post(url, data=data)
-
-    print(f"Your email is: {email}")
-    print(response.text)
+    rqst = requests.post(url, data=value)
+    print(rqst.text)
