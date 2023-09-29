eace4a03bc59cb57db0743f264e83136a848d59e
Ahmedkel <ahmedkeloch@gmail.com>
2023-09-29 19:46:04 +0100

    sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""sends a request to the URL and
+displays the body of the response."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    print(response.text)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
