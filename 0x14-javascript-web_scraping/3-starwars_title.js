b770997121b0b61484366e3d9d4329e7be3a3ece
Ahmedkel <ahmedkeloch@gmail.com>
2023-10-24 15:58:09 +0100

    script that prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
